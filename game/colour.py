45bcf603fe99172c6a260d748ac21bcd1a65e23a
martendo7 <51766598+martendo7@users.noreply.github.com>
2021-03-30 16:51:54 -0400

    Use pygame.Color for colour constants

--- game/colour.py
+++ game/colour.py
@@ -1,5 +1,7 @@
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
+from pygame import Color
+
+BLACK = Color(0, 0, 0)
+WHITE = Color(255, 255, 255)
 
 # This colour should not be seen
-PLACEHOLDER = (255, 0, 255)
+PLACEHOLDER = Color(255, 0, 255)
