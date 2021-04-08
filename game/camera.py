1ecd223fbae3b75aa490235c908d8632e8f223d6
martendo7 <51766598+martendo7@users.noreply.github.com>
2021-04-08 15:09:39 -0400

    Camera: Follow player higher up on the screen

--- game/camera.py
+++ game/camera.py
@@ -3,10 +3,12 @@ import pygame
 class Camera:
     def __init__(self, game):
         self.game = game
+        self.Y_OFFSET = self.game.HEIGHT_PX / 3
+        self.X_OFFSET = self.game.WIDTH_PX / 2
         self.pos = pygame.Vector2(0, 0)
     
     def update(self, follow_rect):
-        x = follow_rect.centerx - (self.game.WIDTH_PX / 2)
+        x = follow_rect.centerx - self.X_OFFSET
         map_width_px = self.game.map.width * self.game.TILE_SIZE
         if x < 0:
             self.pos.x = 0
@@ -15,7 +17,7 @@ class Camera:
         else:
             self.pos.x = x
         
-        y = follow_rect.centery - (self.game.HEIGHT_PX / 2)
+        y = follow_rect.centery - self.Y_OFFSET
         map_height_px = self.game.map.height * self.game.TILE_SIZE
         if y < 0:
             self.pos.y = 0
